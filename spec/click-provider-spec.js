eb61a10c6ebb88f989d6df15723ae10396a6860f
aminya <aminyahyaabadi74@gmail.com>
2020-07-20 19:40:03 -0500

    chore: npm run format

--- spec/click-provider-spec.js
+++ spec/click-provider-spec.js
@@ -1,35 +1,35 @@
-'use babel';
+"use babel";
 
-import { ClickProvider } from '../lib/clickProvider';
-import * as goToDefinition from '../lib/goToDefinition';
+import { ClickProvider } from "../lib/clickProvider";
+import * as goToDefinition from "../lib/goToDefinition";
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
-describe('click provider', () => {
+describe("click provider", () => {
   const editor = atom.workspace.getActiveTextEditor();
   const providerRegistry = {
     getProviderForEditor: () => {},
   };
   const definitions = [
     {
-      language: 'Lang',
-      path: '/path/to/file',
+      language: "Lang",
+      path: "/path/to/file",
       position: { row: 0, column: 0 },
       range: { start: { row: 0, column: 0 }, end: { row: 0, column: 1 } },
     },
   ];
-  const word = 'foo';
+  const word = "foo";
   const range = { start: { row: 0, column: 0 }, end: { row: 0, column: 3 } };
 
   let clickProvider;
-  beforeEach(function() {
-    spyOn(atom.config, 'get').andCallFake(function(value) {
+  beforeEach(function () {
+    spyOn(atom.config, "get").andCallFake(function (value) {
       const config = {
-        'atom-ide-definitions.clickGrammarScopes': [],
-        'atom-ide-definitions.clickPriority': 0,
+        "atom-ide-definitions.clickGrammarScopes": [],
+        "atom-ide-definitions.clickPriority": 0,
       };
       return config[value];
     });
@@ -37,17 +37,17 @@ describe('click provider', () => {
     clickProvider = new ClickProvider(providerRegistry);
   });
 
-  it('verify provider structure', () => {
+  it("verify provider structure", () => {
     const provider = clickProvider.getProvider();
 
     expect(provider.priority).toEqual(0);
     expect(provider.grammarScopes).toBeNull();
-    expect(typeof provider.getSuggestionForWord).toEqual('function');
+    expect(typeof provider.getSuggestionForWord).toEqual("function");
   });
 
-  it('getSuggestionForWord returns range and callback if definition is available', async () => {
+  it("getSuggestionForWord returns range and callback if definition is available", async () => {
     const provider = clickProvider.getProvider();
-    spyOn(goToDefinition, 'getDefinitions').andCallFake(() => definitions);
+    spyOn(goToDefinition, "getDefinitions").andCallFake(() => definitions);
 
     const suggestionForWord = await provider.getSuggestionForWord(
       editor,
@@ -56,12 +56,12 @@ describe('click provider', () => {
     );
 
     expect(suggestionForWord.range).toEqual(range);
-    expect(typeof suggestionForWord.callback).toEqual('function');
+    expect(typeof suggestionForWord.callback).toEqual("function");
   });
 
-  it('getSuggestionForWord returns false if definition is unavailable', async () => {
+  it("getSuggestionForWord returns false if definition is unavailable", async () => {
     const provider = clickProvider.getProvider();
-    spyOn(goToDefinition, 'getDefinitions').andCallFake(() => null);
+    spyOn(goToDefinition, "getDefinitions").andCallFake(() => null);
 
     const suggestionForWord = await provider.getSuggestionForWord(
       editor,
